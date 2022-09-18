959f9532df73726d9b1bdd1296c03375985013cb
kushan2k <kushangayantha001@gmail.com>
2022-09-18 16:48:10 +0530

    home created

--- app/src/main/java/com/webblog/learn101/MainActivity.java
+++ app/src/main/java/com/webblog/learn101/MainActivity.java
@@ -1,8 +1,20 @@
 package com.webblog.learn101;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.media.FaceDetector;
+import android.media.Image;
+import android.media.ImageReader;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
